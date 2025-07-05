8318bd39994ca8103223f22f870e2db6ad82a4f5
GuptaShubham-11 <shu919847@gmail.com>
2025-07-05 20:23:28 +0530

    format code

--- server/models/group.model.ts
+++ server/models/group.model.ts
@@ -1,36 +1,36 @@
-import mongoose, { Schema, Document, Types } from "mongoose";
+import mongoose, { Schema, Document, Types } from 'mongoose';
 
 export interface IGroup extends Document {
-    name: string;
-    owner: Types.ObjectId;
-    members: Types.ObjectId[];
-    createdAt: Date;
-    updatedAt: Date;
+  name: string;
+  owner: Types.ObjectId;
+  members: Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const GroupSchema = new Schema<IGroup>(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        owner: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        members: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "User",
-            },
-        ],
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        timestamps: true,
-    }
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    members: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Group = mongoose.model<IGroup>("Group", GroupSchema);
+const Group = mongoose.model<IGroup>('Group', GroupSchema);
 export default Group;
