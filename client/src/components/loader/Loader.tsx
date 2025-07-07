82639ea9ae8679a9683b91fb8bdcff8eacff8c74
GuptaShubham-11 <shu919847@gmail.com>
2025-07-07 14:36:00 +0530

    add Loader component with CSS animations for loading indicator

---
+++ client/src/components/loader/Loader.tsx
@@ -0,0 +1,13 @@
+import './Loader.css';
+
+const Loader = () => {
+    return (
+        <div className="bar-loader">
+            <div className="bar bar1"></div>
+            <div className="bar bar2"></div>
+            <div className="bar bar3"></div>
+        </div>
+    );
+};
+
+export default Loader;
