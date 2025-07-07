0ee657e1887ce2054b59652b0a973d429fd1ae05
GuptaShubham-11 <shu919847@gmail.com>
2025-07-07 08:28:48 +0530

    add public route

---
+++ client/src/components/PublicRoute.tsx
@@ -0,0 +1,8 @@
+import { Navigate } from 'react-router-dom';
+import { useAuthStore } from '../store/authStore';
+import type { ReactNode } from 'react';
+
+export default function PublicRoute({ children }: { children: ReactNode }) {
+    const { user } = useAuthStore();
+    return !user ? <>{children}</> : <Navigate to="/dashboard" replace />;
+}
