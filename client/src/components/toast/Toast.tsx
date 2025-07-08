ce5432deaa559439d303fd29ed0218568910b8e7
GuptaShubham-11 <shu919847@gmail.com>
2025-07-08 08:40:29 +0530

    code format

--- client/src/components/toast/Toast.tsx
+++ client/src/components/toast/Toast.tsx
@@ -2,17 +2,19 @@ import { CheckCheck } from 'lucide-react';
 import './Toast.css';
 
 interface ToastProps {
-    message: string;
-    type: 'success' | 'error';
+  message: string;
+  type: 'success' | 'error';
 }
 
 const Toast = ({ message, type }: ToastProps) => {
-    return (
-        <div className={`toast ${type}`}>
-            <span className="toast-icon">{type === 'success' ? <CheckCheck /> : '🔴'}</span>
-            <span className="toast-msg">{message}</span>
-        </div>
-    );
+  return (
+    <div className={`toast ${type}`}>
+      <span className="toast-icon">
+        {type === 'success' ? <CheckCheck /> : '🔴'}
+      </span>
+      <span className="toast-msg">{message}</span>
+    </div>
+  );
 };
 
 export default Toast;
