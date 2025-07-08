7d35206e71b0f0c2a90a89a350c48fb9bf843c2e
GuptaShubham-11 <shu919847@gmail.com>
2025-07-08 08:41:44 +0530

    code format

--- client/src/components/loader/Loader.tsx
+++ client/src/components/loader/Loader.tsx
@@ -1,13 +1,13 @@
 import './Loader.css';
 
 const Loader = () => {
-    return (
-        <div className="bar-loader">
-            <div className="bar bar1"></div>
-            <div className="bar bar2"></div>
-            <div className="bar bar3"></div>
-        </div>
-    );
+  return (
+    <div className="bar-loader">
+      <div className="bar bar1"></div>
+      <div className="bar bar2"></div>
+      <div className="bar bar3"></div>
+    </div>
+  );
 };
 
 export default Loader;
