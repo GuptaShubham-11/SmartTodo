775004ab925087ce86740ee63686abff52f0bc14
GuptaShubham-11 <shu919847@gmail.com>
2025-07-13 14:39:14 +0530

    format apis

--- client/src/api/dashboardApi.ts
+++ client/src/api/dashboardApi.ts
@@ -1,44 +1,44 @@
-import apiClient from "./apiClient";
+import apiClient from './apiClient';
 
 const getDashboardData = async () => {
-    try {
-        const response = await apiClient.get('/dashboards/');
-        return response.data;
-    } catch (error: any) {
-        throw error.response.data;
-    }
+  try {
+    const response = await apiClient.get('/dashboards/');
+    return response.data;
+  } catch (error: any) {
+    throw error.response.data;
+  }
 };
 
 const getPopularBoards = async () => {
-    try {
-        const response = await apiClient.get('/dashboards/popular-boards');
-        return response.data;
-    } catch (error: any) {
-        throw error.response.data;
-    }
+  try {
+    const response = await apiClient.get('/dashboards/popular-boards');
+    return response.data;
+  } catch (error: any) {
+    throw error.response.data;
+  }
 };
 
 const getTaskStatusRatio = async () => {
-    try {
-        const response = await apiClient.get('/dashboards/task-status-ratio');
-        return response.data;
-    } catch (error: any) {
-        throw error.response.data;
-    }
+  try {
+    const response = await apiClient.get('/dashboards/task-status-ratio');
+    return response.data;
+  } catch (error: any) {
+    throw error.response.data;
+  }
 };
 
 const getUserActivity = async () => {
-    try {
-        const response = await apiClient.get('/dashboards/user-activity');
-        return response.data;
-    } catch (error: any) {
-        throw error.response.data;
-    }
+  try {
+    const response = await apiClient.get('/dashboards/user-activity');
+    return response.data;
+  } catch (error: any) {
+    throw error.response.data;
+  }
 };
 
 export const dashboardApi = {
-    getDashboardData,
-    getPopularBoards,
-    getTaskStatusRatio,
-    getUserActivity
-}
+  getDashboardData,
+  getPopularBoards,
+  getTaskStatusRatio,
+  getUserActivity,
+};
