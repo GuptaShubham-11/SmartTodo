db1aaad2811913e0f4a8c6361bfcc116729eca6f
GuptaShubham-11 <shu919847@gmail.com>
2025-07-13 14:47:28 +0530

    add index file for page exports

---
+++ client/src/pages/index.ts
@@ -0,0 +1,6 @@
+import Group from './group/Group';
+import Board from './board/Board';
+import KanbanBoard from './kanbanBoard/KanbanBoard';
+import Dashboard from './dashboard/Dashboard';
+
+export { Group, Board, KanbanBoard, Dashboard };
