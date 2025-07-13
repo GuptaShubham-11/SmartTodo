8b213a94a92173166d37beb4ee1dd053ede9e235
GuptaShubham-11 <shu919847@gmail.com>
2025-07-13 14:46:29 +0530

    add index for export

---
+++ client/src/components/index.ts
@@ -0,0 +1,35 @@
+import Signup from './signup/Signup';
+import PublicRoute from './PublicRoute';
+import ProtectedRoute from './ProtectedRoute';
+import Loader from './loader/Loader';
+import Signin from './signin/Signin';
+import Header from './header/Header';
+import Layout from './layout/Layout';
+import CreateGroupModal from './group/CreateGroupModal';
+import GroupCard from './group/GroupCard';
+import BoardCard from './board/BoardCard';
+import CreateBoardModal from './board/CreateBoardModal';
+import CreateTaskModal from './kanbanBoard/CreateTaskModal';
+import TaskCard from './kanbanBoard/TaskCard';
+import MemberModal from './kanbanBoard/MemberModal';
+import ActionLogModal from './kanbanBoard/ActionLogModal';
+import GroupMemberModal from './group/GroupMemberModal';
+
+export {
+  Signup,
+  PublicRoute,
+  ProtectedRoute,
+  Loader,
+  Signin,
+  Header,
+  Layout,
+  CreateGroupModal,
+  GroupCard,
+  BoardCard,
+  CreateBoardModal,
+  CreateTaskModal,
+  TaskCard,
+  MemberModal,
+  ActionLogModal,
+  GroupMemberModal,
+};
