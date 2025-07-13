7a919cfe5673bc2ecca03f3562d02b7988504499
GuptaShubham-11 <shu919847@gmail.com>
2025-07-13 14:42:21 +0530

    add bar loader

--- client/src/components/loader/Loader.tsx
+++ client/src/components/loader/Loader.tsx
@@ -2,7 +2,7 @@ import './Loader.css';
 
 const Loader = () => {
   return (
-    <div className="bar-loader">
+    <div className="bar-loader" role="status" aria-label="Loading">
       <div className="bar bar1"></div>
       <div className="bar bar2"></div>
       <div className="bar bar3"></div>
