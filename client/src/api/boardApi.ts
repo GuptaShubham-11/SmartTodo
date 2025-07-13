3ab418e255176b3e534d1206aa488fc3ed72b620
GuptaShubham-11 <shu919847@gmail.com>
2025-07-13 14:38:00 +0530

    add apis

---
+++ client/src/api/boardApi.ts
@@ -0,0 +1,84 @@
+import apiClient from "./apiClient";
+
+const createBoard = async (data: any) => {
+    try {
+        const response = await apiClient.post('/boards/', data);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+const getBoardsByGroupId = async (groupId: string) => {
+    try {
+        const response = await apiClient.get(`/boards/group/${groupId}`);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+const getBoardById = async (boardId: string) => {
+    try {
+        const response = await apiClient.get(`/boards/${boardId}`);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+const updateBoard = async (boardId: string, data: any) => {
+    try {
+        const response = await apiClient.put(`/boards/${boardId}`, data);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+const deleteBoard = async (boardId: string) => {
+    try {
+        const response = await apiClient.delete(`/boards/${boardId}`);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+const addMemberToBoard = async (boardId: string, data: any) => {
+    try {
+        const response = await apiClient.put(`/boards/add-member/${boardId}`, data);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+const removeMemberFromBoard = async (boardId: string, data: any) => {
+    try {
+        const response = await apiClient.put(`/boards/remove-member/${boardId}`, data);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+const getMembersByBoard = async (boardId: string) => {
+    try {
+        const response = await apiClient.get(`/boards/${boardId}/members`);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+const getActionLogs = async (boardId: string) => {
+    try {
+        const response = await apiClient.get(`/actionlogs/board/${boardId}`);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+export const boardApi = { createBoard, getBoardsByGroupId, getBoardById, updateBoard, deleteBoard, addMemberToBoard, removeMemberFromBoard, getMembersByBoard, getActionLogs };
---
+++ client/src/api/groupApi.ts
@@ -0,0 +1,55 @@
+import apiClient from './apiClient';
+
+const createGroup = async (data: any) => {
+    try {
+        const response = await apiClient.post('/groups/create-group', data);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+const getUserGroups = async () => {
+    try {
+        const response = await apiClient.get('/groups/user-groups');
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+
+    }
+}
+
+const getGroupById = async (groupId: string) => {
+    try {
+        const response = await apiClient.get(`/groups/${groupId}`);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+const addMemberToGroup = async (boardId: string, userId: string) => {
+    try {
+        const response = await apiClient.put(`/groups/add-member/${boardId}`, { userId });
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+const removeMemberFromGroup = async (boardId: string, userId: string) => {
+    try {
+        const response = await apiClient.put(`/groups/remove-member/${boardId}`, userId);
+        return response.data;
+    } catch (error: any) {
+        throw error.response.data;
+    }
+}
+
+export const groupApi = {
+    createGroup,
+    getUserGroups,
+    getGroupById,
+    addMemberToGroup,
+    removeMemberFromGroup
+}
